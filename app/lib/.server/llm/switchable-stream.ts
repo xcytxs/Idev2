1bb6f994e14d3122068066496ab6c9040315f7f8
Anirban Kar <thecodacus@gmail.com>
2024-11-16 13:41:36 +0530

    chore: reverted the log as it was in main

--- app/lib/.server/llm/switchable-stream.ts
+++ app/lib/.server/llm/switchable-stream.ts
@@ -47,6 +47,7 @@ export default class SwitchableStream extends TransformStream {
         this._controller.enqueue(value);
       }
     } catch (error) {
+      console.log(error);
       this._controller.error(error);
     }
   }
