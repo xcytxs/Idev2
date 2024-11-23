4ff27498d75c1268ce65a32988ede5f49472b5cc
Dustin Loring <dustinwloring1988@gmail.com>
2024-11-23 17:38:39 -0500

    replaced placeholder text in main messagebox

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -254,7 +254,7 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
                       minHeight: TEXTAREA_MIN_HEIGHT,
                       maxHeight: TEXTAREA_MAX_HEIGHT,
                     }}
-                    placeholder="How can Bolt help you today?"
+                    placeholder="How can oTToDev help you today?"
                     translate="no"
                   />
                   <ClientOnly>
